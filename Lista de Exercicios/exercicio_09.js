c6456762356c90a42f9ddfd774c26f31e0e7aac0
bruno jaen <brunojaen@outlook.com.br>
2022-10-07 12:01:20 -0300

    Exercicio de for com array

---
+++ Lista de Exercicios/exercicio_09.js
@@ -0,0 +1,10 @@
+// 02) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Pars encontrados
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i <= numeros.length; i++) {
+    const numero = numeros[i];
+    if (numero % 2 === 0) {
+        console.log(numero);
+    }
+}
