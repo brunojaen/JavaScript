9d842c0fe0835b7cf652a23816c0118e7fe8a882
bruno jaen <brunojaen@outlook.com.br>
2022-10-07 08:44:13 -0300

    Exercicio e Aula sobre objetos

--- Lista de Exercicios/exercicio_05.js
+++ Lista de Exercicios/exercicio_05.js
@@ -1,21 +1,34 @@
 
-function valorDesconto(valor, desconto) {
-    return (valor - (valor * (desconto / 100)));
+function main() {
+
+    function valorDesconto(valor, desconto) {
+        return (valor - (valor * (desconto / 100)));
+    }
+
+    function valorJuros(valor, juros) {
+        return (valor + (valor * (juros / 100)));
+    }
+
+    const precoEtiqueta = 100;
+    const formaDePagamento = 1;
+
+    if (formaDePagamento === 1) {
+        console.log(valorDesconto(precoEtiqueta, 10));
+    } else if (formaDePagamento === 2) {
+        console.log(valorDesconto(precoEtiqueta, 15));
+    } else if (formaDePagamento === 3) {
+        console.log(precoEtiqueta)
+    } else {
+        console.log(valorJuros(precoEtiqueta, 10));
+    }
 }
 
-function valorJuros(valor, juros) {
-    return (valor + (valor *(juros / 100)));
+main();
+
+
+
+function calcularMedia(nota1, nota2) {
+    const media = (nota1 + nota2) / 2;
 }
 
-const precoEtiqueta = 100;
-const formaDePagamento = 4;
-
-if (formaDePagamento === 1) {
-    console.log(valorDesconto(precoEtiqueta, 10));
-} else if (formaDePagamento === 2) {
-    console.log(valorDesconto(precoEtiqueta, 15));
-} else if (formaDePagamento === 3) {
-    console.log(precoEtiqueta)
-} else {
-    console.log(valorJuros(precoEtiqueta, 10));
-}
+console.log(calcularMedia(5, 5));
